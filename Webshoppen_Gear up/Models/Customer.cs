4a42a251b3b7cd2d670558482d8eb29f855cec17
Stefrisk <stefrisk@gmail.com>
2025-02-03 13:30:16 +0100

    added methods -  find customer and new customer

--- Webshoppen_Gear up/Models/Customer.cs
+++ Webshoppen_Gear up/Models/Customer.cs
@@ -20,14 +20,85 @@ namespace Webshoppen_Gear_up.Models
         public int Phone { get; set; }
         public int? OrderID { get; set; }
         public int? ShoppingCartID { get; set; }
-         
-        
 
 
+
+        public Customer(string name, int age, string country, string city, int postalcode, string streetaddress, string email, int phone)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+            City = city;
+            PostalCode = postalcode;
+            StreetAddress = streetaddress;
+            Email = email;
+            Phone = phone;
+
+        }
         
         
         public virtual ICollection<Order>? Orders { get; set; } = new List<Order>();
         public virtual Shopping_Cart? Shopping_Cart { get; set; }
+
+
+        public static void NewCustomer()
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------\nCreate new customer | Please enter the following information: ");
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Country:");
+            string country = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("Postalcode:");
+            int postal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Street Address:");
+            string street = Console.ReadLine();
+            Console.WriteLine("Email:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Phone number:");
+            int phone = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Customer newCustomer = new Customer(name, age, country, city, postal, street, email, phone);
+                var db = new GearUpContext();
+                db.Customers.Add(newCustomer);
+                db.SaveChanges();
+            }
+            catch
+            {
+                Console.WriteLine("Something when wrong, please try again.");
+                
+            }
+
+        }
+
+        public static int FindCustomer()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your name: ");
+                string customer = Console.ReadLine();
+                var db = new GearUpContext();
+                var result = db.Customers.SingleOrDefault(x => x.Name == customer);
+                if (result != null)
+                {
+                    return result.CustomerID;
+                }
+                else
+                {
+                    Console.WriteLine("Could not find customer, try again");
+                    
+                }
+            }
+                
+        }
+
+       
+
         
     }
 }
