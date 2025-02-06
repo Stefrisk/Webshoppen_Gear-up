c30cf212538b62fdccd1c13d57eb9690b80e2b2f
Stefrisk <stefrisk@gmail.com>
2025-02-06 23:44:54 +0100

    added methods for add update and remove

--- Webshoppen_Gear up/Models/Category.cs
+++ Webshoppen_Gear up/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,69 @@ namespace Webshoppen_Gear_up.Models
     {
         public int CategoryID { get; set; }
         public string Name { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public Category() { }
+        public Category(string name)
+        {
+            Name = name; 
+        }
+
+        public static void ChangeCat() 
+        {
+            var db = new GearUpContext();
+
+            Console.WriteLine("enter a category ID: ");
+            Int32.TryParse(Console.ReadLine(), out int choice);
+            if (choice > 0)
+            {
+                var result = db.Categories.SingleOrDefault(x => x.CategoryID == choice);
+                Console.WriteLine("What would you like to change the name of the category to?");
+                string newName = Console.ReadLine();
+                if (result != null) 
+                {
+                    result.Name = newName;
+                    db.SaveChanges();
+                }
+                else { Console.WriteLine("name cannot be null must contain characters"); }
+
+            }
+            else { Console.WriteLine("Choice must be greater that 0 and not negative"); }
+        } 
+        
+        public static void AddCat()
+        {
+            var db = new GearUpContext();
+            Console.WriteLine("Enter name of new category:  ");
+            string newCat = Console.ReadLine();
+            if (newCat != null)
+            {
+                Category category = new Category(newCat);
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+            else { Console.WriteLine("Invaild category name! Cannot be null must contain characters!"); }
+        }
+        public static void RemoveCat() 
+        {
+            var db = new GearUpContext();
+
+            Console.WriteLine("Enter a category ID: ");
+            Int32.TryParse(Console.ReadLine(), out int choice);
+            if (choice > 0)
+            {
+                var result = db.Categories.SingleOrDefault(x => x.CategoryID == choice);
+                if (result != null) 
+                { 
+                    result.IsDeleted = true;
+                    db.Categories.Remove(result);
+                    db.SaveChanges();
+                }
+                else { Console.WriteLine("Category not found!"); }
+               
+            }
+            else { Console.WriteLine("Choice must be greater that 0 and not negative"); }
+        }
         
     }
 }
