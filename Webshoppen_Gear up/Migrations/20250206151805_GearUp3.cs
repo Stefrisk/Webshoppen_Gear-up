766255e857c6a9ad7671cbb52b180f617abdb6bf
Stefrisk <stefrisk@gmail.com>
2025-02-06 16:29:21 +0100

    added item quanity to item class

---
+++ Webshoppen_Gear up/Migrations/20250206151805_GearUp3.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webshoppen_Gear_up.Migrations
+{
+    /// <inheritdoc />
+    public partial class GearUp3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCart_Customers_CustomerID",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCart_CustomerID",
+                table: "ShoppingCart");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerID",
+                table: "ShoppingCart",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderQuantity",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCart_CustomerID",
+                table: "ShoppingCart",
+                column: "CustomerID",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCart_Customers_CustomerID",
+                table: "ShoppingCart",
+                column: "CustomerID",
+                principalTable: "Customers",
+                principalColumn: "CustomerID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCart_Customers_CustomerID",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCart_CustomerID",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropColumn(
+                name: "OrderQuantity",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerID",
+                table: "ShoppingCart",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCart_CustomerID",
+                table: "ShoppingCart",
+                column: "CustomerID",
+                unique: true,
+                filter: "[CustomerID] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCart_Customers_CustomerID",
+                table: "ShoppingCart",
+                column: "CustomerID",
+                principalTable: "Customers",
+                principalColumn: "CustomerID");
+        }
+    }
+}
